028fe680dfc821f779ac7ffe5feb6e633a1713af
Simon Fuchs <sfuchs42@gmail.com>
2019-10-31 21:18:26 +0000

    Added stripe for payments

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
  def enrolled_in?(course)
  	return enrolled_courses.include?(course)
