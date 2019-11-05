7b68002341ac596864290569d7ae514e0c03972d
Simon Fuchs <sfuchs42@gmail.com>
2019-11-05 19:50:43 +0000

    Section ordering js

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create] 
+      resources :sections, only: [:new, :create]
     end
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
