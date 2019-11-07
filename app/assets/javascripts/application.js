ec3e662abd78ff86905d6e63bb7542f13f3d2199
Simon Fuchs <sfuchs42@gmail.com>
2019-11-07 01:05:48 +0000

    Changed order in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require tether
 //= require jquery-ui
+//= require_tree .
